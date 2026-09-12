689ccd4d91185a1ed38dafbd72f10b330c5b7723
saiganeshpalla <ganesh@stockholmitacademy.org>
2026-09-12 10:59:52 +0530

    added code

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import "./App.css";
 function App() {
   const [products, setProducts] = useState([]);
   const [selectedProduct, setSelectedProduct] = useState(null);
+  console.log("hello world")
 
   useEffect(() => {
     fetch("https://fakestoreapi.com/products")
